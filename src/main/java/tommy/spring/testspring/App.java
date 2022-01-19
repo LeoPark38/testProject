5e196dbe95a3c4933e5aae711fe36125f161110f
DESKTOP-36BD3PV\Leo <packc38@gmail.com>
2022-01-19 20:20:00 +0900

    폴더 붙여넣기

---
+++ src/main/java/tommy/spring/testspring/App.java
@@ -0,0 +1,13 @@
+package tommy.spring.testspring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
